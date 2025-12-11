c16c59ccd20e05255f495d8a013f0d0d524ca45d
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-12-10 23:30:51 -0800

    make code more legible

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::{
-    fs::{remove_file, rename, File},
-    io::{copy, Read},
-    path::{Path, PathBuf},
-};
-
 use clap::Parser;
 use log::{error, info, trace, warn, LevelFilter};
+use std::{
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
 use tempfile::NamedTempFile;
 
 #[derive(Parser, Debug)]
@@ -23,7 +22,7 @@ const UTF8_BOM: [u8; 3] = [0xEF, 0xBB, 0xBF];
 fn main() -> Result<(), anyhow::Error> {
     env_logger::builder().filter(None, LevelFilter::Info).init();
     let args = Params::parse_from(wild::args());
-    let mut last_error: Option<anyhow::Error> = None;
+    let mut exit_result = Ok(());
     let mut count = 0;
 
     for filename in &args.files {
@@ -37,39 +36,38 @@ fn main() -> Result<(), anyhow::Error> {
             continue;
         }
 
-        if let Err(e) = remove_bom(&file, filename, args.nobackup) {
-            last_error = Some(e);
+        let result = remove_bom(&file, filename, args.nobackup);
+        if result.is_err() {
+            exit_result = result;
             continue;
-        }        
+        }
         count += 1;
         trace!("done processing");
     }
     info!("{} file(s) processed", count);
 
-    if let Some(e) = last_error { Err(e) } else { Ok(()) }
+    exit_result
 }
 
 fn remove_bom(mut file: &File, filename: &Path, nobackup: bool) -> Result<(), anyhow::Error> {
     info!("processing {}...", filename.display());
     let mut tempfile = create_tempfile(filename, file)?;
-    copy(&mut file, &mut tempfile)
-        .inspect_err(|_| error!("cannot write to the temporary file"))?;
+    std::io::copy(&mut file, &mut tempfile).inspect_err(|_| error!("cannot write to the temporary file"))?;
     let bak_filename = filename.with_extension("bak");
-    rename(filename, &bak_filename)
+    std::fs::rename(filename, &bak_filename)
         .inspect_err(|_| error!("cannot create the backup file '{}'", filename.display()))?;
-    if rename(tempfile.path(), filename).is_err() {
+    if std::fs::rename(tempfile.path(), filename).is_err() {
         error!(
             "cannot rename the temporary file to '{}'",
             filename.display()
         );
-        rename(bak_filename, filename).inspect_err(|_| {
-            error!("cannot restore the backup file '{}'", filename.display())
-        })?;
+        std::fs::rename(bak_filename, filename)
+            .inspect_err(|_| error!("cannot restore the backup file '{}'", filename.display()))?;
         return Err(anyhow::anyhow!("cannot rename the temporary file"));
     }
 
     if nobackup {
-        remove_file(&bak_filename).inspect_err(|_| {
+        std::fs::remove_file(&bak_filename).inspect_err(|_| {
             error!("cannot remove the backup file '{}'", bak_filename.display())
         })?;
     }
@@ -78,12 +76,12 @@ fn remove_bom(mut file: &File, filename: &Path, nobackup: bool) -> Result<(), an
 
 fn has_bom<R: Read>(source: &mut R) -> bool {
     let mut buffer = [0; 3];
-    if source.read_exact(&mut buffer).inspect_err(|e| {
-        warn!("cannot read the BOM: {}", e);
-    }).is_err() {
-        return false;
-    }
-    buffer == UTF8_BOM
+    source
+        .read_exact(&mut buffer)
+        .inspect_err(|e| {
+            warn!("cannot read the BOM: {}", e);
+        })
+        .is_ok() && buffer == UTF8_BOM
 }
 
 fn create_tempfile(filename: &Path, input_file: &File) -> Result<NamedTempFile, anyhow::Error> {
